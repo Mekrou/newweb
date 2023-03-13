d9fdd61f1cb00c9e82d4420a048c3f59a01ced92
Mekrou <mills.ryan.011@gmail.com>
2023-03-13 12:15:19 -0400

    Okay. Script working now

--- newweb.sh
+++ newweb.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 NAME=$1
-TEMPLATE="~/Scripts/js-template/"
+TEMPLATE="${HOME}/Scripts/js-template/"
 STARTING_DIR=$(pwd)
 
 # make new dir with name user specified
-mkdir "NAME"
+mkdir $NAME
 
-INDEX_LOC=$TEMPLATEindex.html
-APP_LOC=$TEMPLATEapp.js
+INDEX_LOC="${TEMPLATE}index.html"
+APP_LOC="${TEMPLATE}app.js"
+
+echo $INDEX_LOC
+echo $APP_LOC
+echo $STARTING_DIR
 
 # copy default template into new folder
-cp $INDEX_LOC $APP_LOC $STARTING_DIR 
+cp $INDEX_LOC $APP_LOC $STARTING_DIR/$NAME 
 
 #open new folder in vscode
 code $STARTING_DIR/$NAME
